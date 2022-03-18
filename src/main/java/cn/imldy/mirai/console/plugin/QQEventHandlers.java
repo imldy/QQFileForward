be03bff8fb15a8635754a166773b7584ad915af0
imldy <im.ldy@outlook.com>
2022-03-18 23:44:21 +0800

    fix-转发失败（手动下载再上传）

--- src/main/java/cn/imldy/mirai/console/plugin/QQEventHandlers.java
+++ src/main/java/cn/imldy/mirai/console/plugin/QQEventHandlers.java
@@ -2,13 +2,21 @@ package cn.imldy.mirai.console.plugin;
 
 import cn.imldy.mirai.console.plugin.conf.MyConf;
 import cn.imldy.mirai.console.plugin.conf.Patterns;
+import com.ejlchina.okhttps.HTTP;
+import kotlin.coroutines.CoroutineContext;
 import net.mamoe.mirai.contact.Group;
+import net.mamoe.mirai.contact.file.AbsoluteFile;
+import net.mamoe.mirai.contact.file.AbsoluteFolder;
+import net.mamoe.mirai.contact.file.RemoteFiles;
 import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.SimpleListenerHost;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 import net.mamoe.mirai.message.data.FileMessage;
 import net.mamoe.mirai.message.data.SingleMessage;
+import net.mamoe.mirai.utils.ExternalResource;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.util.regex.Pattern;
 
 /**
@@ -16,24 +24,82 @@ import java.util.regex.Pattern;
  * @date 2022/03/18 16:16
  **/
 public class QQEventHandlers extends SimpleListenerHost {
+    HTTP http = HTTP.builder().build();
+
+    @Override
+    public void handleException(@NotNull CoroutineContext context, @NotNull Throwable exception) {
+        // super.handleException(context, exception);
+        System.out.println("出错了");
+        exception.printStackTrace();
+    }
 
     @EventHandler
     public void onGroupMessageEvent(GroupMessageEvent event) {
         if (!matchSourceGroup(event.getGroup()))
             return;
 
-        SingleMessage fileMessage = null;
+        FileMessage fileMessage = null;
         for (SingleMessage singleMessage : event.getMessage()) {
             if (singleMessage instanceof FileMessage) {
                 if (matchFileMessage((FileMessage) singleMessage))
-                    fileMessage = singleMessage;
+                    fileMessage = (FileMessage) singleMessage;
             }
         }
-        assert fileMessage != null;
+
+        // 确定本地文件存放目录
+        File path = FileForward.INSTANCE.resolveDataFile("/量化");
+        File toDayFile = new File(path + "/" + fileMessage.getName());
+
+        // 下载文件
+        AbsoluteFile absoluteFile = fileMessage.toAbsoluteFile(event.getGroup());
+
+        String url = absoluteFile.getUrl();
+
+        downloadFile(url, toDayFile);
+        // 上传文件
+
         Group targetGroup = event.getBot().getGroup(MyConf.conf.getTargetGroup());
         assert targetGroup != null;
-        targetGroup.sendMessage(fileMessage);
+
+        uploadAndSend(targetGroup, toDayFile, fileMessage.getName());
+
     }
+
+    /**
+     *
+     * @param url
+     * @param destFile
+     */
+    private void downloadFile(String url, File destFile) {
+        http.sync(url)
+                .get()
+                .getBody()
+                .toFile(destFile)
+                .start();
+    }
+
+    /**
+     * 上传到目标群并命名
+     *
+     * @param targetGroup 目标群
+     * @param localFile   本地文件
+     * @param remoteName  上传后的命名
+     */
+    private void uploadAndSend(Group targetGroup, File localFile, String remoteName) {
+
+        ExternalResource resource = ExternalResource.create(localFile);
+        // 文件标识
+        RemoteFiles files = targetGroup.getFiles();
+        AbsoluteFolder root = files.getRoot();
+        // AbsoluteFolder folder = root.createFolder("/量化");
+
+        AbsoluteFile uploadNewFile = root.uploadNewFile(remoteName, resource);
+
+        // 好像不需要执行发送消息的步骤
+        // FileMessage fileMessage1 = uploadNewFile.toMessage();
+        // targetGroup.sendMessage(fileMessage1);
+    }
+
 
     /**
      * 判断消息来源群组是否满足要求
